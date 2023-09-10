73c5947140c7e01ffbed43eb58d9c817f28dac4a
harrdas <bsnn2944@gmail.com>
2023-09-11 00:21:23 +0900

    - add: 카드 종류에 따라 색상 다르게 표시

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,3 +1,4 @@
+import { CardTypes } from "@/app/types/CardTypes";
 import React from "react";
 
 export default function Card({
@@ -9,12 +10,27 @@ export default function Card({
   card: any;
   handleClick: any;
 }) {
+  const getCardColor = (cardTypes: CardTypes) => {
+    switch (cardTypes) {
+      case "spade":
+        return "text-blue-950";
+      case "heart":
+        return "text-pink-500";
+      case "diamond":
+        return "text-red-500";
+      case "club":
+        return "text-black";
+      default:
+        return "text-purple-500	";
+    }
+  };
+
   return (
     <div
       key={index}
-      className={`cursor-pointer w-16 h-24 bg-gray-100 rounded-md flex flex-col justify-center items-center ${
-        card.color === "red" ? "text-red-500" : "text-black-500"
-      }`}
+      className={`cursor-pointer w-16 h-24 bg-gray-100 rounded-md flex flex-col justify-center items-center ${getCardColor(
+        card.type
+      )}`}
       onClick={handleClick}
     >
       <div className="text-base">{card.type}</div>
