eef69e463e7afe2b7dfd15d8bba82dbee134b05b
harrdas <bsnn2944@gmail.com>
2023-09-17 18:49:34 +0900

    - add: 카드 종류별로 state 생성 - add: 카드 초기화 하는 함수 생성하여 사용

--- app/page.tsx
+++ app/page.tsx
@@ -6,47 +6,44 @@ import {
   CardNumberTypes,
   CardTypes,
 } from "@/app/types/CardTypes";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
-  const [cards, setCards] = useState<CardInterface[]>([]);
-  const [clubCards, setClubCards] = useState<CardInterface[]>([]);
-  const [spadeCards, setSpadeCards] = useState<CardInterface[]>([]);
-  const [heartCards, setHeartCards] = useState<CardInterface[]>([]);
-  const [diamondCards, setDiamondCards] = useState<CardInterface[]>([]);
+  const cardNumbers: CardNumberTypes[] = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
 
-  useEffect(() => {
-    const cardTypes: CardTypes[] = ["club", "spade", "heart", "diamond"];
-    const cardNumbers: CardNumberTypes[] = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K",
-    ];
+  const initializeCards = (type: CardTypes) =>
+    cardNumbers.map((cardNumber) => ({
+      type: type,
+      number: cardNumber,
+      isToggled: false,
+    }));
 
-    // get all Cards
-    cardTypes.forEach((cardType) => {
-      cardNumbers.forEach((cardNumber) => {
-        setCards((prev) => [
-          ...prev,
-          {
-            type: cardType,
-            number: cardNumber,
-            isToggled: false,
-          },
-        ]);
-      });
-    });
-  }, []);
+  const [clubCards, setClubCards] = useState<CardInterface[]>(
+    initializeCards("club")
+  );
+  const [spadeCards, setSpadeCards] = useState<CardInterface[]>(
+    initializeCards("spade")
+  );
+  const [heartCards, setHeartCards] = useState<CardInterface[]>(
+    initializeCards("heart")
+  );
+  const [diamondCards, setDiamondCards] = useState<CardInterface[]>(
+    initializeCards("diamond")
+  );
 
   const handleClick = () => {};
 
@@ -54,8 +51,38 @@ export default function Home() {
     <div className="p-4 max-w-7xl mx-auto">
       <h1 className="text-4xl text-center">카드 카운팅</h1>
       <br />
-      <div className="flex flex-row flex-wrap gap-4">
-        {cards.map((card, index) => (
+      <div className="flex flex-row flex-wrap gap-4 mb-6">
+        {clubCards.map((card, index) => (
+          <Card
+            key={`${card.type}-${index}`}
+            index={index}
+            card={card}
+            handleClick={handleClick}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row flex-wrap gap-4 mb-6">
+        {spadeCards.map((card, index) => (
+          <Card
+            key={`${card.type}-${index}`}
+            index={index}
+            card={card}
+            handleClick={handleClick}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row flex-wrap gap-4 mb-6">
+        {heartCards.map((card, index) => (
+          <Card
+            key={`${card.type}-${index}`}
+            index={index}
+            card={card}
+            handleClick={handleClick}
+          />
+        ))}
+      </div>
+      <div className="flex flex-row flex-wrap gap-4 mb-6">
+        {diamondCards.map((card, index) => (
           <Card
             key={`${card.type}-${index}`}
             index={index}
